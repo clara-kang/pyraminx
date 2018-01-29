a10f18926a20def1c4908876b15760e4353f1a37
Clara Kang <heng.kang@mail.mcgill.ca>
2018-01-29 17:54:53 +0100

    rotate around normal of subpyraminx

--- js/pyraminx.js
+++ js/pyraminx.js
@@ -91,14 +91,13 @@ scene.add( arrowHelperX );
 scene.add( arrowHelperY );
 scene.add( arrowHelperZ );
 
+// to intersect objects
 var raycaster = new THREE.Raycaster();
 var mouse = new THREE.Vector2();
 
 function onMouseMove( event ) {
-
 	// calculate mouse position in normalized device coordinates
 	// (-1 to +1) for both components
-
 	mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
 	mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
 }
@@ -115,11 +114,10 @@ function restaurelastIntersectObj(lastIntersectObj) {
 }
 
 var lastIntersectObj = null;
-// the one close to origin, the one on +x, the one on +z, the one on +y
-var normal1 = new THREE.Vector3(2 / Math.sqrt(6), 1 / 3, 2 / Math.sqrt(18));
-var normal2 = new THREE.Vector3(-2 / Math.sqrt(6), 1 / 3, 2 / Math.sqrt(18));
-var normal3 = new THREE.Vector3(0, 1 / 3, -1);
-var normal4 = new THREE.Vector3(0, -1, 0);
+var normal1 = new THREE.Vector3(2 / Math.sqrt(6), 1 / 3, 2 / Math.sqrt(18)); // the one close to origin
+var normal2 = new THREE.Vector3(-2 / Math.sqrt(6), 1 / 3, 2 / Math.sqrt(18)); //the one on +x
+var normal3 = new THREE.Vector3(0, 1 / 3, -4 / Math.sqrt(18)); //the one on +z
+var normal4 = new THREE.Vector3(0, -1, 0); //the one on +y
 
 var subPyraminx = [
   {tetras: [0, 3, 6, 1], normal: normal1.normalize(), point: new THREE.Vector3(0, 0, 0)},// the one close to origin
@@ -130,74 +128,60 @@ var subPyraminx = [
 var indexToRotate = 3;
 var animating = false;
 var rotationProgress = 0;
-window.addEventListener("keypress", function(event) {
-  switch(event.key) {
-    case "a":
-      indexToRotate = 0;
-      break;
-    case "s":
-      indexToRotate = 1;
-      break;
-    case "d":
-      indexToRotate = 2;
-      break;
-    case "f":
-      indexToRotate = 3;
-      break;
-  }
-  animating = true;
-  rotationProgress = 0;
-  // switch (indexToRotate) {
-  //   case 0:
-  //     console.log("0!");
-  //     subPyraminx[2].tetras[3] = subPyraminx[0].tetras[3];
-  //     subPyraminx[3].tetras[1] = subPyraminx[0].tetras[1];
-  //     subPyraminx[1].tetras[1] = subPyraminx[0].tetras[2];
-  //     console.log(subPyraminx[2].tetras);
-  //     break;
-  //   case 1:
-  //     console.log("1!");
-  //     subPyraminx[2].tetras[1] = subPyraminx[1].tetras[1];
-  //     subPyraminx[3].tetras[3] = subPyraminx[1].tetras[3];
-  //     subPyraminx[0].tetras[4] = subPyraminx[1].tetras[2];
-  //     break;
-  //   case 2:
-  //     console.log("2!");
-  //     subPyraminx[1].tetras[3] = subPyraminx[2].tetras[2];
-  //     subPyraminx[0].tetras[1] = subPyraminx[2].tetras[1];
-  //     subPyraminx[3].tetras[2] = subPyraminx[2].tetras[3];
-  //     break;
-  //   case 3:
-  //     console.log("3!");
-  //     subPyraminx[0].tetras[2] = subPyraminx[3].tetras[2];
-  //     subPyraminx[1].tetras[2] = subPyraminx[3].tetras[1];
-  //     subPyraminx[2].tetras[2] = subPyraminx[3].tetras[3];
-  //     break;
-  // }
-});
 
 var dAngle;
 var angleToRotate = Math.PI * 2 / 3; // amount to rotate subPyraminx per click
 var tetraTriggerIndex = [0, 2, 5, 9]; // the index of tetrahedrons that triggers subPyraminx rotation when clicked on
 var tetraIndex = -1; // intersected tetrahedron index in tetrahedrons array
 
+// the actual position of the tetrahedrons to their index in the tetrahedrons array
+// initially the same, change as subPyraminx rotates
+var positionToIndex = new Map();
+for (i = 0; i < 10; i++) {
+  positionToIndex.set(i, i);
+}
+// click to rotate subPyraminx
 window.addEventListener("click", function( event ) {
+  console.log("tetraIndex: " + tetraIndex);
+  console.log("animating: " + animating);
+  // if not currently rotating any subPyraminx, and the mouse is on a tetrahedron
   if (animating == false && tetraIndex != -1) {
+    // determine which subPyraminx to rotate based on the tetrahedron trigger
     switch (tetraIndex) {
-      case 0:
-        indexToRotate = 0;
-        break;
-      case 2:
-        indexToRotate = 1;
-        break;
-      case 5:
-        indexToRotate = 2;
-        break;
-      case 9:
-        indexToRotate = 3;
-        break;
-    }
-    // restaure opacity for rotating
+    case 0: // the one close to origin
+      indexToRotate = 0;
+      var backup1 = positionToIndex.get(1);
+      var backup3 = positionToIndex.get(3);
+      positionToIndex.set(3, positionToIndex.get(6));
+      positionToIndex.set(6, backup1);
+      positionToIndex.set(1, backup3);
+      break;
+    case 2: //the one on +x
+      indexToRotate = 1;
+      var backup4 = positionToIndex.get(4);
+      var backup7 = positionToIndex.get(7);
+      positionToIndex.set(4, positionToIndex.get(1));
+      positionToIndex.set(7, backup4);
+      positionToIndex.set(1, backup7);
+      break;
+    case 5: //the one on +z
+      indexToRotate = 2;
+      var backup3 = positionToIndex.get(3);
+      var backup8 = positionToIndex.get(8);
+      positionToIndex.set(3, positionToIndex.get(4));
+      positionToIndex.set(8, backup3);
+      positionToIndex.set(4, backup8);
+      break;
+    case 9: //the one on +y
+      indexToRotate = 3;
+      var backup6 = positionToIndex.get(6);
+      var backup7 = positionToIndex.get(7);
+      positionToIndex.set(6, positionToIndex.get(8));
+      positionToIndex.set(7, backup6);
+      positionToIndex.set(8, backup7);
+      break;
+  }
+    // restore opacity for rotation
     for (i = 0; i < 10; i++) {
       for (j = 0; j < 4; j++) {
         tetrahedrons[i].material[j].opacity = 1;
@@ -208,6 +192,11 @@ window.addEventListener("click", function( event ) {
   }
 });
 
+// matrices for transformation in the rotation
+// todo: optimize
+var m2= new THREE.Matrix4();
+var m3= new THREE.Matrix4();
+var m4= new THREE.Matrix4();
 // render
 function animate() {
   // if subPyraminx rotating and not finished
@@ -220,9 +209,12 @@ function animate() {
     // rotate each tetrahedron in the subPyraminx
     for (pyrIndex of subPyraminx[indexToRotate].tetras) {
       //rotate the tetrahedron along the axis defined by the normal and the point
-      tetrahedrons[pyrIndex].position.sub(subPyraminx[indexToRotate].point);
-      tetrahedrons[pyrIndex].rotateOnWorldAxis ( subPyraminx[indexToRotate].normal, dAngle );
-      tetrahedrons[pyrIndex].position.add(subPyraminx[indexToRotate].point);
+      m2.makeRotationAxis ( subPyraminx[indexToRotate].normal, dAngle )
+      m3.makeTranslation ( -subPyraminx[indexToRotate].point.x, -subPyraminx[indexToRotate].point.y, -subPyraminx[indexToRotate].point.z )
+      m4.makeTranslation ( subPyraminx[indexToRotate].point.x, subPyraminx[indexToRotate].point.y, subPyraminx[indexToRotate].point.z )
+      tetrahedrons[positionToIndex.get(pyrIndex)].applyMatrix ( m3 );
+      tetrahedrons[positionToIndex.get(pyrIndex)].applyMatrix ( m2 );
+      tetrahedrons[positionToIndex.get(pyrIndex)].applyMatrix ( m4 );
     }
     // update rotation progress
     rotationProgress += dAngle;
@@ -247,7 +239,7 @@ function animate() {
             // if yes, lower the opacity of all tetrahedrons in the subPyraminx
             for (pyrIndex of subPyrIndexList.tetras) {
               for (j = 0; j < 4; j++) {
-          		  tetrahedrons[pyrIndex].material[j].opacity = 0.5;
+          		  tetrahedrons[positionToIndex.get(pyrIndex)].material[j].opacity = 0.5;
               }
             }
           }
